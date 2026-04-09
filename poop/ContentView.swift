d0af0bf87e56df96d7be2695be7dde466614bc6e
vkpdeveloper <vaibhavpathak9984@gmail.com>
2026-04-10 02:42:08 +0530

    Initial Commit

---
+++ poop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poop
+//
+//  Created by Vaibhav on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poop/poopApp.swift
@@ -0,0 +1,17 @@
+//
+//  poopApp.swift
+//  poop
+//
+//  Created by Vaibhav on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct poopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
